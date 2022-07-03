e55279af28609364c7ace10c058ebfa803412ea1
luohancai <530176577@qq.com>
2022-07-04 00:21:07 +0800

    feat: 增加前端代码

---
+++ ssoms/vue/src/App.vue
@@ -0,0 +1,93 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <!-- <HelloWorld msg="Hello Vue 3 + Vite" /> -->
+  <a-layout>
+    <a-layout-header class="header__bar">
+      <div class="header__logo">
+        <img alt="Vue logo" src="./assets/logo.png" />
+        <h1>单点管理系统</h1>
+      </div>
+    </a-layout-header>
+    <a-layout>
+      <a-layout-sider theme="light" class="sider__bar">
+        <a-menu
+          v-model:selectedKeys="state.selectedKeys"
+          v-model:openKeys="state.openKeys"
+          mode="inline"
+          :style="{ height: '100%', borderRight: 0 }"
+        >
+        <a-menu-item key="1">
+          <bars-outlined />
+          <router-link to="/menu">对象管理</router-link>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <audit-outlined />
+          <router-link to="/user">用户管理</router-link>
+        </a-menu-item>
+        <a-menu-item key="5">
+          <appstore-outlined />
+          <router-link to="/user">角色管理</router-link>
+        </a-menu-item>
+        <a-menu-item key="6">
+          <audit-outlined />
+          <router-link to="/user">授权管理</router-link>
+        </a-menu-item>
+        </a-menu>
+      </a-layout-sider>
+      <a-layout-content class="main__bar">Content</a-layout-content>
+    </a-layout>
+    <a-layout-footer>Footer</a-layout-footer>
+  </a-layout>
+</template>
+<script setup lang="ts">
+// import HelloWorld from './components/HelloWorld.vue'
+import { reactive } from 'vue'
+import {
+  HomeOutlined,
+  BarsOutlined,
+  AuditOutlined,
+  AppstoreOutlined,
+  LoadingOutlined,
+} from '@ant-design/icons-vue'
+
+const state = reactive({
+  selectedKeys: [],
+  openKeys: []
+})
+</script>
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+.header__bar.ant-layout-header{
+  height: 48px;
+  background-color: #fff;
+  box-shadow: 0 0 8px 0 rgb(0 0 0 / 10%);
+  display: flex;
+  justify-content: space-between;
+  z-index: 999;
+}
+.header__logo{
+  display: flex;
+  align-items: center;
+}
+.header__logo img{
+  width: 24px;
+  height: 24px;
+}
+.header__logo h1{
+  font-size: 15px;
+  margin: 0 0 0 8px;
+  color: #262626;
+}
+.sider__bar.ant-layout-sider-light, .sider__bar .ant-menu{
+  height: calc(100vh - 48px);
+  background-color: #f8f8f8;
+}
+
+.main__bar{
+  background-color: #fff;
+  padding: 30px;
+}
+</style>
---
+++ ssoms/vue/src/router/index.ts
@@ -0,0 +1,23 @@
+import VueRouter from 'vue-router'
+import User from '../views/user.vue'
+
+// 1. 定义路由组件.
+// 也可以从其他文件导入
+const Home = { template: '<div>Home</div>' }
+
+// 2. 定义一些路由
+// 每个路由都需要映射到一个组件。
+// 我们后面再讨论嵌套路由。
+const routes = [
+  { path: '/', component: Home },
+  { path: '/user', component: User },
+]
+
+// 3. 创建路由实例并传递 `routes` 配置
+// 你可以在这里输入更多的配置，但我们在这里
+// 暂时保持简单
+const router = VueRouter.createRouter({
+  // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
+  history: VueRouter.createWebHashHistory(),
+  routes
+})
